1261b76fa23ad1bca8f4bc7c75092aed3a628af7
matymorales <maty.morales.linux@gmail.com>
2026-09-16 19:53:07 -0300

    fix para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://varano-detail.pages.dev",
   vite: {
     plugins: [tailwindcss()],
   },
